365a064b28ade15fa53bea2890e36063c4fc71df
Matt Morris <mcm0027@yahoo.com>
2016-01-22 23:19:29 +0000

    add validation for presence and length to description

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -48,4 +48,40 @@ describe "Creating to do lists" do
         visit "/todo_lists"
         expect(page).to_not have_content("This is what I'm doing today.")
     end
+    
+    it "displays an error when todo list has no description." do
+        expect(TodoList.count).to eq(0)
+    
+        visit  "/todo_lists"
+        click_link "New Todo list"
+        expect(page).to have_content("New Todo List")
+        
+        fill_in "Title", with: "Hello"
+        fill_in "Description", with: ""
+        click_button "Create Todo list"
+        
+        expect(page).to have_content("error")
+        expect(TodoList.count).to eq(0)
+        
+        visit "/todo_lists"
+        expect(page).to_not have_content("Hello")
+    end
+    
+    it "displays an error when todo list has less than 5 char description." do
+        expect(TodoList.count).to eq(0)
+    
+        visit  "/todo_lists"
+        click_link "New Todo list"
+        expect(page).to have_content("New Todo List")
+        
+        fill_in "Title", with: "Hello"
+        fill_in "Description", with: "what"
+        click_button "Create Todo list"
+        
+        expect(page).to have_content("error")
+        expect(TodoList.count).to eq(0)
+        
+        visit "/todo_lists"
+        expect(page).to_not have_content("Hello")
+    end
 end
