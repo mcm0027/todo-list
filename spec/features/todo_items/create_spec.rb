753719b1c5e48a3895b2eed7b1c9a888c64aeb7f
Matt Morris <mcm0027@yahoo.com>
2016-01-25 00:57:35 +0000

    Add delete item functionality and spec

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -19,6 +19,27 @@ describe "Adding to do items" do
         within('ul.todo_items') do
             expect(page).to have_content("Milk") 
         end
-        
+    end
+    
+    it "displays an error when no content" do
+        visit_todo_list(todo_list)
+        click_link "New Todo Item"
+        fill_in "Content", with: ""
+        click_button "Save"
+        within("div.flash") do
+            expect(page).to have_content("There was a problem adding that item.")
+        end
+        expect(page).to have_content("Content can't be blank")
+    end
+    
+     it "displays an error when less than 2 chars" do
+        visit_todo_list(todo_list)
+        click_link "New Todo Item"
+        fill_in "Content", with: "a"
+        click_button "Save"
+        within("div.flash") do
+            expect(page).to have_content("There was a problem adding that item.")
+        end
+        expect(page).to have_content("Content is too short")
     end
 end
